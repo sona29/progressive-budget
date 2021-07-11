e88af108d887faf9e8bd716b472ae4ed3233d103
Sona Shrestha <sona.shrestha004@gmail.com>
2021-07-11 19:19:00 +1000

    adding online links to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,9 @@ const FILES_TO_CACHE = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
+  "/font-awesome.min.css",
   "/db.js",
+  "/index.js",
   "/styles.css",
   "/icons/icon-72x72.png",
   "/icons/icon-96x96.png",
@@ -14,6 +16,8 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-384x384.png",
   "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
 ];
 
 // install
